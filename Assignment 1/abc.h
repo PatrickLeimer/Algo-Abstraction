b74400d73febde3200218c68a6262f79d5708d9b
Julio A. Leonardi <jalg00065@gmail.com>
2026-01-30 15:37:40 -0500

    Tiny fix for task a, made my draft for task c

---
+++ Assignment 1/abc.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+vector<vector<int>> gsMatching(int n, vector<vector<int>> hospital_pref, vector<vector<int>> student_pref);
+void testGen(int base, int exponent);
