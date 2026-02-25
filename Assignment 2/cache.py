6b1a16e4cb22b9ee558b6b9d10dd498056cd65c0
Julio A. Leonardi <jalg00065@gmail.com>
2026-02-25 16:06:17 -0500

    added a message for future reference

--- Assignment 2/cache.py
+++ Assignment 2/cache.py
@@ -1,6 +1,8 @@
 #I dont know how to do linked lists in python
 from queue import Queue
 
+
+#TODO: Add a function to FIFO and LRU that handles the entire list of requests. 
 class FIFO:
     def __init__(self, k):
         self.k = k
@@ -30,12 +32,15 @@ class LRU: #I would normaly do a linked list with a hashmap for this, Ill just c
     def request(self, r):
         if r in self.cache:
             self.cache.remove(r)
-            self.cache.append(r)
+            self.cache.append(r) #add at the end
             return True
         
         if len(self.cache) == self.k:
-            self.cache.pop(0)
+            self.cache.pop(0) #remove at the beggining
             
         self.cache.append(r)
         self.miss_count += 1
         return False
+    
+class OPTFF: #This approach uses information from future requests, so all requests must be handled/looked at at the same time
+    pass
