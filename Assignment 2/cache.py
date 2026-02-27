24112f08e507ce5e347c80def313e08006d3368f
JulioLeonardi <jalg00065@gmail.com>
2026-02-26 19:11:42 -0500

    finished the OPTFF and corrected a typo in FIFO

--- Assignment 2/cache.py
+++ Assignment 2/cache.py
@@ -6,12 +6,12 @@ from queue import Queue
 class FIFO:
     def __init__(self, k):
         self.k = k
-        self.queue = Queue(maxsize = 3) #https://www.geeksforgeeks.org/python/queue-in-python/
+        self.queue = Queue(maxsize = k) #https://www.geeksforgeeks.org/python/queue-in-python/
         self.set = set() #for checking if an element is in the queue in O(1) time
         self.miss_count = 0
     
     def request(self, r):
-        if r in set:
+        if r in self.set:
             return True
         
         if self.queue.full():
@@ -49,26 +49,46 @@ class LRU: #I would normaly do a linked list with a hashmap for this, Ill just c
     def handleRequests(self, rl): 
         for r in rl:
             self.request(r)
-    
+
+#I am guessing an optimal approach would have some form of max heap, I dont want to do allat  
 class OPTFF: #This approach uses information from future requests, so all requests must be handled/looked at at the same time
     def __init__(self, k):
         self.cache = []
         self.miss_count = 0
         self.k = k
 
-    # def computeDistances(self, rl):
-    #     for i <  lenrl:
-    #         for
-
     def handleRequests(self, rl):
         for i  in range(len(rl)):
             r = rl[i]
             if r in self.cache:
                 continue
+
+            self.miss_count +=1
                 
-            if len(self.cache) == self.k:
-                max = 0
-                value = 0
-                for j in range(i, len(rl)):
-                    if rl[j] 
+            if len(self.cache) < self.k:
+                 self.cache.append(r)
+                 continue
+
+            max_dist = -1
+            value = None
+
+            for s in self.ache: #n^2 ahh
+                try: #trying to avoid out of range exceptions
+                    j = rl.index(s, i + 1)
+                    dist = j - i
+                except ValueError:
+                    dist = float('inf')
+
+                if dist > max_dist:
+                    max_dist = dist
+                    value = s
+                
+            self.cache.remove(value)
+            self.cache.append(r)
+            
+                
+           
+            
+                
+                    
 
